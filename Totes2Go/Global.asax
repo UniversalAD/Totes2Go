63724ca8fdc30211f60c17345c1ffc35e7168b2a
Chris Goodrow <goodrow.chris4@gmail.com>
2018-02-20 11:50:21 -0500

    Add project files.

---
+++ Totes2Go/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Totes2Go.MvcApplication" Language="C#" %>
---
+++ Totes2Go/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Totes2Go.Startup))]
+namespace Totes2Go
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
